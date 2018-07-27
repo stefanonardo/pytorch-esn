8765e8d77ef20394f691cae3ea249094501b241b
Stefano Nardo <stefano_nardo@msn.com>
2018-07-27 18:47:05 +0200

    version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pytorch-esn',
-      version='1.1.0',
+      version='1.1.1',
       packages=find_packages(),
       install_requires=[
           'torch',
